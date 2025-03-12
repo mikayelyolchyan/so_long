ea8e5fd2785daabe30165dcccd0080df3325b784
mikayelyolchyan <mikayelyolchyan@gmail.com>
2025-03-12 21:17:59 +0400

    add start_game function

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 #include "so_long.h"
 
-int main(void)
+int	start_game(t_data *data)
 {
-	t_data	data;
-
-	data.mlx_ptr = mlx_init();
-	if (!data.mlx_ptr)
+	data->mlx_ptr = mlx_init();
+	if (!data->mlx_ptr)
 	{
 		ft_putstr_fd("MiniLibX initialization Error\n", 1);
 		return (1);
 	}
 
-	data.win_ptr = mlx_new_window(data.mlx_ptr, 1024, 1024, "Simple Window");
-	if (!data.win_ptr)
+	data->win_ptr = mlx_new_window(data->mlx_ptr, 1024, 1024, "Simple Window");
+	if (!data->win_ptr)
 	{
 		ft_putstr_fd("Window creating Error\n", 1);
 		return (1);
 	}
+	return (0);
+}
 
+int main(void)
+{
+	t_data	data;
+
+	if (start_game(&data) == 1)
+		return (1);
 	mlx_string_put(data.mlx_ptr, data.win_ptr, 512, 512, 0xFFFFFF, "PACMAN");
 	
 	mlx_hook(data.win_ptr, 17, 1L << 2, ft_exit, &data);
