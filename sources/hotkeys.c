1526f21175ead304c7bc87f0cc1a5af84f34f6a4
nosoftplayer <nosoftplayer@kali>
2025-04-02 16:48:21 +0400

    pac moving frame_delay

--- sources/hotkeys.c
+++ sources/hotkeys.c
@@ -5,22 +5,22 @@ int	ft_hotkey(int keycode, t_game *game)
 	if (keycode == W && game->map->map[(game->player->y - 32) / 32][(game->player->x) / 32] != '1')
 	{
 		game->player->direction = UP;
-		pac_moving(game);
+		//pac_moving(game);
 	}
 	else if (keycode == S && game->map->map[(game->player->y + 32) / 32][(game->player->x) / 32] != '1')
 	{
 		game->player->direction = DOWN;
-		pac_moving(game);
+		//pac_moving(game);
 	}
 	else if (keycode == A && game->map->map[(game->player->y) / 32][(game->player->x - 32) / 32] != '1')
 	{
 		game->player->direction = LEFT;
-		pac_moving(game);
+		//pac_moving(game);
 	}
 	else if (keycode == D && game->map->map[(game->player->y) / 32][(game->player->x + 32) / 32] != '1')
 	{
 		game->player->direction = RIGHT;
-		pac_moving(game);
+		//pac_moving(game);
 	}
 	else if (keycode == ESC)
 	{
