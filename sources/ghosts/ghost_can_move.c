fa493b2c75f3f55e07da137da6663e9e2f3cfcc2
nosoftplayer <nosoftplayer@kali>
2025-04-12 18:26:41 +0400

    norminette

--- sources/ghosts/ghost_can_move.c
+++ sources/ghosts/ghost_can_move.c
@@ -1,45 +1,45 @@
 #include "../../includes/headers/so_long.h"
 
-int ghost_can_move_vertical(t_ghost *c_ghost, char **map, int direction)
+int	ghost_can_move_vertical(t_ghost *c_ghost, char **map, int direction)
 {
-    if (direction == UP && map[(c_ghost->y - 4) / 32] \
+	if (direction == UP && map[(c_ghost->y - 4) / 32] \
 		[c_ghost->x / 32] != '1' && c_ghost->x % 32 == 0)
-        return (1);
-    if (direction == DOWN && map[(c_ghost->y + 4) / 32 + 1] \
+		return (1);
+	if (direction == DOWN && map[(c_ghost->y + 4) / 32 + 1] \
 		[c_ghost->x / 32] != '1' && c_ghost->x % 32 == 0)
-        return (1);
-    return (0);
+		return (1);
+	return (0);
 }
 
-int ghost_can_move_horizontal(t_ghost *c_ghost, char **map, int direction)
+int	ghost_can_move_horizontal(t_ghost *c_ghost, char **map, int direction)
 {
-    if (direction == LEFT && map[c_ghost->y / 32] \
+	if (direction == LEFT && map[c_ghost->y / 32] \
 		[(c_ghost->x - 4) / 32] != '1' && c_ghost->y % 32 == 0)
-        return (1);
-    if (direction == RIGHT && map[c_ghost->y / 32] \
+		return (1);
+	if (direction == RIGHT && map[c_ghost->y / 32] \
 		[(c_ghost->x + 4) / 32 + 1] != '1' && c_ghost->y % 32 == 0)
-        return (1);
-    return (0);
+		return (1);
+	return (0);
 }
 
-int ghost_can_move_fast_vertical(t_ghost *c_ghost, char **map, int direction)
+int	ghost_can_move_fast_vertical(t_ghost *c_ghost, char **map, int direction)
 {
-    if (direction == UP && map[(c_ghost->y - 16) / 32] \
+	if (direction == UP && map[(c_ghost->y - 16) / 32] \
 		[c_ghost->x / 32] != '1' && c_ghost->x % 32 == 0)
-        return (1);
-    if (direction == DOWN && map[(c_ghost->y + 16) / 32 + 1] \
+		return (1);
+	if (direction == DOWN && map[(c_ghost->y + 16) / 32 + 1] \
 		[c_ghost->x / 32] != '1' && c_ghost->x % 32 == 0)
-        return (1);
-    return (0);
+		return (1);
+	return (0);
 }
 
-int ghost_can_move_fast_horizontal(t_ghost *c_ghost, char **map, int direction)
+int	ghost_can_move_fast_horizontal(t_ghost *c_ghost, char **map, int direction)
 {
-    if (direction == LEFT && map[c_ghost->y / 32] \
+	if (direction == LEFT && map[c_ghost->y / 32] \
 		[(c_ghost->x - 16) / 32] != '1' && c_ghost->y % 32 == 0)
-        return (1);
-    if (direction == RIGHT && map[c_ghost->y / 32] \
+		return (1);
+	if (direction == RIGHT && map[c_ghost->y / 32] \
 		[(c_ghost->x + 16) / 32 + 1] != '1' && c_ghost->y % 32 == 0)
-        return (1);
-    return (0);
+		return (1);
+	return (0);
 }
