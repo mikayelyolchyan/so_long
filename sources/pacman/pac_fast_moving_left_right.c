a7eb473ffc92c8f3efba37f69fa4d123704ca46b
nosoftplayer <nosoftplayer@kali>
2025-04-12 16:47:53 +0400

    fixed bug pac eating portal

--- sources/pacman/pac_fast_moving_left_right.c
+++ sources/pacman/pac_fast_moving_left_right.c
@@ -15,7 +15,7 @@ static void	fast_move_right(t_game *game)
 		game->player->x += 8;
 	else
 		game->player->x = (game->player->x + 8) / 32 * 32;
-	if (game->map->map[game->player->y / 32][game->player->x / 32] == 'T')
+	if (game->map->map[game->player->y / 32][(game->player->x + 32 )/ 32] == 'T')
 		handle_portal(game, 0);
 }
 
--- sources/pacman/pac_moving_left_right.c
+++ sources/pacman/pac_moving_left_right.c
@@ -4,7 +4,7 @@ static void	move_left(t_game *game)
 {
 	if (game->map->map[game->player->y / 32][(game->player->x - 4) / 32] != '1')
 		game->player->x -= 4;
-	if (game->map->map[game->player->y / 32][game->player->x / 32] == 'T')
+	if (game->map->map[game->player->y / 32][(game->player->x - 8) / 32] == 'T')
 		handle_portal(game, 1);
 }
 
@@ -15,7 +15,7 @@ static void	move_right(t_game *game)
 		game->player->x += 4;
 	else
 		game->player->x = (game->player->x + 4) / 32 * 32;
-	if (game->map->map[game->player->y / 32][game->player->x / 32] == 'T')
+	if (game->map->map[game->player->y / 32][(game->player->x + 32) / 32] == 'T')
 		handle_portal(game, 0);
 }
 
